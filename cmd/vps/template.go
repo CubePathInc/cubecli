48f575ff8c733828e6ace8b5e1c30345c5f6b3ec
Marc Iglesias <marc@cubepath.com>
2026-03-29 18:48:32 +0200

    Fix vps template list to use /vps/templates endpoint with token auth

--- cmd/vps/template.go
+++ cmd/vps/template.go
@@ -23,7 +23,7 @@ func addTemplateCmd(parent *cobra.Command) {
 
 			s := output.NewSpinner("Fetching templates...")
 			s.Start()
-			resp, err := client.Get("/pricing")
+			resp, err := client.Get("/vps/templates")
 			s.Stop()
 			if err != nil {
 				return err
@@ -33,24 +33,35 @@ func addTemplateCmd(parent *cobra.Command) {
 				return output.PrintJSON(json.RawMessage(resp))
 			}
 
-			var pricing struct {
-				VPS struct {
-					Templates []struct {
-						Name    string `json:"template_name"`
-						OS      string `json:"os_name"`
-						Version string `json:"version"`
-					} `json:"templates"`
-				} `json:"vps"`
+			var result struct {
+				OperatingSystems []struct {
+					TemplateName string `json:"template_name"`
+					OSName       string `json:"os_name"`
+					Version      string `json:"version"`
+				} `json:"operating_systems"`
+				Applications []struct {
+					AppName         string `json:"app_name"`
+					Version         string `json:"version"`
+					RecommendedPlan string `json:"recommended_plan"`
+				} `json:"applications"`
 			}
-			if err := json.Unmarshal(resp, &pricing); err != nil {
+			if err := json.Unmarshal(resp, &result); err != nil {
 				return fmt.Errorf("failed to parse response: %w", err)
 			}
 
-			t := output.NewTable("VPS Templates", []string{"Template Name", "OS", "Version"})
-			for _, tmpl := range pricing.VPS.Templates {
-				t.AddRow(tmpl.Name, tmpl.OS, tmpl.Version)
+			t := output.NewTable("OS Templates", []string{"Template Name", "OS", "Version"})
+			for _, tmpl := range result.OperatingSystems {
+				t.AddRow(tmpl.TemplateName, tmpl.OSName, tmpl.Version)
 			}
 			t.Render()
+
+			if len(result.Applications) > 0 {
+				a := output.NewTable("Application Templates", []string{"App", "Version", "Recommended Plan"})
+				for _, app := range result.Applications {
+					a.AddRow(app.AppName, app.Version, app.RecommendedPlan)
+				}
+				a.Render()
+			}
 			return nil
 		},
 	}
